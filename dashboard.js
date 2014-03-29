f8200b4fb2e33e64b929485d8b72869a1c58eeda
nothing2lose <nothing.sungpil@gmail.com>
2014-03-29 22:45:47 +0900

    dependacny removed & module version-up patch

--- dashboard.js
+++ dashboard.js
@@ -1,61 +1,68 @@
 var sys     = require('util');
 var express = require('express');
 var twitter = require('ntwitter');
+var http    = require('http');
 
-var app = express.createServer();
+//var app = express.createServer();
+var app = express();
+
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function(req, res, next){
+  //req.sesssion
   res.render('/public/index.html');
 });
-app.listen(process.env.PORT || 20853);
-console.log('Server running at http://localhost:20853/');
+var server = http.createServer(app);
+server.listen(3000, function(){
+  console.log('Server running at http://localhost:3000/');
+});
 
-var io  = require('socket.io').listen(app);
+var io  = require('socket.io').listen(server);
 
 myList = [];
 Array.prototype.del = function(val) {
-    for(var i=0; i<this.length; i++) {
-        if(this[i] == val) {
-            this.splice(i, 1);
-            break;
-        }
+  for(var i=0; i<this.length; i++) {
+    if(this[i] == val) {
+      this.splice(i, 1);
+      break;
     }
+  }
 }
 
 CreateTwitter();
 io.sockets.on('connection', function(socket) {
-    socket.on('data', function(action,data) {
-	console.log(action);
-	
-	if(action==='+') {
-        	myList.push(data);
-			console.log(data);
-	}
-	else {
-		myList.del(data);
-	}
+  socket.on('data', function(action,data) {
+    console.log('????????');
+    console.log(action);
+
+    if(action==='+') {
+      myList.push(data);
+      console.log(data);
+    }
+    else {
+      myList.del(data);
+    }
+  });
+  socket.on('getfilter', function() {
+    socket.emit('pushfilter', myList);
+    console.log('emit pushfilter');
+  });
+  if(myList.length!=0) {
+    twit.stream('user',{track:myList}, function(stream) {
+      stream.on('data', function (tweet) {
+        socket.emit('message', JSON.stringify(tweet));
+      });
     });
-    socket.on('getfilter', function() {
-        socket.emit('pushfilter', myList);
-		console.log('emit pushfilter');
-    });
-    if(myList.length!=0) {
-        twit.stream('user',{track:myList}, function(stream) {
-            stream.on('data', function (tweet) {
-  	    	    socket.emit('message', JSON.stringify(tweet));
-            });
-        });
-    }   
+  }
 });
 
 function CreateTwitter() {
-twit = new twitter({
-    consumer_key: '40msDUOpu4Mh1r6rvglig',
-    consumer_secret: 'h6G692nS42tUIAZsnpss2dtflQ8ec88k30ckJhkY0',
-    access_token_key: '13604142-I1IAdVelSCLZg5uXBqgzKb8hnlM6PoVOXSGl9x2FI',
-    access_token_secret: '6SZ14gn1KUctjO2sN2PfY2EinYAoYhLJ4GuSLXSd4'
-});
+  twit = new twitter({
+    consumer_key: 'up1J5O2oBeKdg8ajI8L9Xw',
+    consumer_secret: 'zxgGcTVXKTXPelzunK8cpNA1DM2fQJf56bLBvm9dE',
+    access_token_key: '92786883-ItXOtdkPLAJstryB4G82q8XMNyOK2Z76znGo2Vrqb',
+    access_token_secret: 'soRNfBdttTJRb3Yk5V22dgJ1FyAbLLJeUgkdbS5sJRyAF'
+  });
 }
